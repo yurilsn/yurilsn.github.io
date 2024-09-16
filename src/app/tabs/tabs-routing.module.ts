2e16d55308cdbff4d476943c09e21d0759d338d8
Yuri Luiz Silva do Nascimento <luizyuri57@gmail.com>
2024-09-16 19:52:19 -0300

    Corrigindo path da aplicação para o github pages

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -4,7 +4,7 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
-    path: 'tabs',
+    path: 'ionic-deep-link-lab/tabs',
     component: TabsPage,
     children: [
       {
@@ -28,7 +28,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: 'ionic-deep-link-lab/tabs/tab1',
     pathMatch: 'full'
   }
 ];
